f6d5136d81f63fdf82930e104b33aa2947eb3923
Stephen Buchanan <stephen@mannmademedia.com>
2015-05-30 15:01:29 +0200

    removed rover directive, using paperjs instead

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -24,6 +24,25 @@ angular.module('goRover')
 
     $scope.go = false;
 
+    paper.setup('board');
+    var vs = paper.view.viewSize;
+
+    var background = new paper.Path.Rectangle({
+      from: [0,0],
+      to: [vs.width, vs.height],
+      fillColor: 'blue'//'#221f26'
+    });
+
+    var rover = new paper.Raster({
+      source: 'assets/images/rover.png', 
+      position: paper.view.center
+    });
+    rover.scale(0.5);
+
+    console.log(rover.image);
+
+    // rover.position = topLeft;
+
     $scope.clickGo = function() {
       console.log('clicked go!');
       $scope.go = !$scope.go;
