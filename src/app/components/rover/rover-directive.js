7e9c7bf80e2b55bdff11f73a2827d7679ec9adee
Stephen Buchanan <stephen@mannmademedia.com>
2015-05-30 14:31:27 +0200

    toggle go value from button, watched by directive

--- src/app/components/rover/rover-directive.js
+++ src/app/components/rover/rover-directive.js
@@ -5,9 +5,13 @@ angular.module('goRover')
     return {
       templateUrl: 'app/components/rover/rover.template.html',
       restrict: 'E',
-      link: function postLink(scope, element) {
+      link: function postLink(scope, element, attrs) {
 
-      	console.log('rover directive loaded!');
+      	console.log('rover directive loaded:', attrs);
+
+      	scope.$watch('go', function() {
+      		console.log('go = ', scope.go);
+      	});
 
       }
     }
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,63 +2,31 @@
 
 angular.module('goRover')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+
+    $scope.commands = [
       {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+        "type": "forward"
       },
       {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
+        "type": "right"
       },
       {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
+        "type": "forward"
       },
       {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
+        "type": "left"
       },
       {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'Foundation',
-        'url': 'http://foundation.zurb.com/',
-        'description': 'The most advanced responsive front-end framework in the world.',
-        'logo': 'foundation.png'
-      },
-      {
-        'title': 'Angular Foundation',
-        'url': 'http://pineconellc.github.io/angular-foundation/',
-        'description': 'A set of native AngularJS directives based on Foundation\'s markup and CSS',
-        'logo': 'angular-foundation.png'
-      },
-      {
-        'title': 'Sass (Node)',
-        'url': 'https://github.com/sass/node-sass',
-        'description': 'Node.js binding to libsass, the C version of the popular stylesheet preprocessor, Sass.',
-        'logo': 'node-sass.png'
+        "type": "forward"
       }
+
     ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
+
+    $scope.go = false;
+
+    $scope.clickGo = function() {
+      console.log('clicked go!');
+      $scope.go = !$scope.go;
+    }
+
   });
